e10b136f2345821ad9121de1c2d1651dc00c9213
AlexDerAndros <ntrikos@gmx.de>
2025-10-03 15:06:35 +0200

    Erstes Design

--- scf/src/App.jsx
+++ scf/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 export default function App() {
   return (
-    <div className="text-pink-500 w-full flex items-center justify-center">
-      Hallo 
-    </div>
+    <>
+      <div className="w-1/6 h-screen p-3 flex flex-col items-left bg-sidebar text-white ">
+        <div className="font-poppins">
+          Learning App
+        </div>
+      </div>
+    </>
   );
 }
 
